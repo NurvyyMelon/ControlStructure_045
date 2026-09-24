7507d0daad218509d85388e8cce1157efeacde68
NurvyyMelon <adinnaufal9@gmail.com>
2026-09-24 10:55:13 +0700

    Menambahkan logika untuk mengevaluasi kinerja berdasarkan persentase yang dimasukkan pengguna

---
+++ StudentPerformanceEvaluation.py
@@ -0,0 +1,11 @@
+percentage = float(input("Masukkan persentase: "))
+if percentage >= 90:
+    print("Performance Yang Sangat Bagus")
+elif percentage >= 80:
+    print("Performance Yang Bagus")
+elif percentage >= 70:
+    print("Performance Yang Cukup")
+elif percentage >= 60:
+    print("Performance Yang Kurang")
+else:
+    print("Performance Yang Perlu Ditingkatkan")
